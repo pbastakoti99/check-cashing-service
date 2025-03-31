aa38a14795051b0180d7676c6eb727ea19e93f51
prabin29 <prabinbastakoti11@gmail.com>
2025-03-31 17:36:53 -0500

    initial commit

---
+++ src/main/java/com/pb/check_cashing_service/CheckCashingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pb.check_cashing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckCashingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckCashingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pb/check_cashing_service/CheckCashingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pb.check_cashing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckCashingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
